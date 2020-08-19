f705c32da742351b5145f2fee09d7ec9b1ab712b
goDlin <godlin086@gmail.com>
2020-08-19 18:16:39 +0300

    Initial Commit

---
+++ TVfootball/TVfootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVfootballApp.swift
+//  TVfootball
+//
+//  Created by Денис on 19.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct TVfootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
