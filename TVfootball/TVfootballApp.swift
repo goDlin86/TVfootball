907a633a11521a7ea96a905c84b0f6e71ebf0914
Denis <godlin086@gmail.com>
2023-05-04 17:55:17 +0300

    terminate app after closed window

--- TVfootball/TVfootballApp.swift
+++ TVfootball/TVfootballApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct TVfootballApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(ScheduleStore(service: .init()))
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
     }
 }
